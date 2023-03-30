7f52a64662c1b258f1578701ce93005c717d00ce
harshgupta20 <hgreatgupta2001@gmail.com>
2023-03-30 23:52:05 +0530

    loader added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import "../styles/Navbar.css";
 
 const Navbar = () => {
@@ -8,9 +7,7 @@ const Navbar = () => {
             <div id="navbar">
                 <div id="nav-body">
                     <div id="nav-title">
-                        {/* <Link to="/"> */}
                             <p id="nav-p1">Tiny Chunk</p>
-                        {/* </Link> */}
                         <p id="nav-p2">(Shorten your web links countlessly)</p>
                     </div>
 
--- src/components/ShortenUrlInput.jsx
+++ src/components/ShortenUrlInput.jsx
@@ -4,26 +4,34 @@ import { doc, setDoc } from "firebase/firestore";
 import { db } from "../config/Firebase";
 import "../styles/ShortenUrlInput.css";
 import tinyid from "tiny-unique-id";
+import { ColorRing } from 'react-loader-spinner'
 
 const ShortenUrlInput = () => {
 
     const [url, setUrl] = useState();
     const [ShortenUrl, setShortenUrl] = useState();
+    const [loader, setLoader] = useState(false);
 
     const handleUrlShorten = async () => {
         const slug = tinyid.unique();
-
-        if(url.slice(0,8) == "https://"){
-            await setDoc(doc(db, "urls", slug), {
-                slug: slug,
-                originalUrl: url
-            });
-            setShortenUrl(`${window.location.href}${slug}`);
-
+        
+        if(url){
+            if (url.slice(0, 8) == "https://") {
+                setLoader(true)
+                await setDoc(doc(db, "urls", slug), {
+                    slug: slug,
+                    originalUrl: url
+                });
+                setShortenUrl(`${window.location.href}${slug}`);
+                
+            } else {
+                alert("Kindly paste url including \"https://\"");
+            }
         }else{
-            alert("Kindly paste url including \"https://\"");
+            alert("Input is empty");
         }
-
+        
+        setLoader(false);
     }
 
 
@@ -39,8 +47,19 @@ const ShortenUrlInput = () => {
                     <input id="short-input" type="text" onChange={(e) => setUrl(e.target.value)} placeholder="Enter URL here..." />
                     <button id="short-btn1" onClick={handleUrlShorten} >Create Link</button>
                 </div>
-                <hr style={{borderBottom:'1px solid black', width:'100%', margin:'30px'}} />
+                <hr style={{ borderBottom: '1px solid black', width: '100%', margin: '30px' }} />
                 <div id="short-url-copy">
+                    <div style={{display: loader ? "block" : "none"}}>
+                        <ColorRing
+                            visible={true}
+                            height="80"
+                            width="80"
+                            ariaLabel="blocks-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="blocks-wrapper"
+                            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+                        />
+                    </div>
                     {
                         ShortenUrl && <h1 id="short-h1">{ShortenUrl}</h1>
                     }
