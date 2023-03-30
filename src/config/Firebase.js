bda594d17cc7ccd5f61bc80ca95283c7ba7abcce
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-03-30 13:38:08 +0530

    firebase setup completed

---
+++ src/config/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcWumiaR_pPYK2pxbTVhNxC4lLo8sNYM0",
+  authDomain: "url-chunk.firebaseapp.com",
+  projectId: "url-chunk",
+  storageBucket: "url-chunk.appspot.com",
+  messagingSenderId: "977942981884",
+  appId: "1:977942981884:web:bfc3f6d7945340d508fb79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
